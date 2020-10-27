8a4c4795a9c8460967fff94ecac8203f7da34615
Ambuj <jack@Ambujs-MacBook-Pro.local>
2020-10-27 08:11:15 +0530

    minor

--- crowdsource/settings.py
+++ crowdsource/settings.py
@@ -119,4 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
+#for heroku
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfile")
 STATIC_URL = '/static/'
